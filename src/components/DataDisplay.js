e0c5b6d4bab239f29edf05001b68f534baba6042
Pedja Stojiljkovic <pedja1310@gmail.com>
2021-12-07 16:12:06 +0100

    data table displaying results properly

--- src/components/DataDisplay.js
+++ src/components/DataDisplay.js
@@ -1,7 +1,36 @@
+import { useSelector } from "react-redux";
 import { Grid } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 
 const DataDisplay = () => {
-  return <Grid item>Data display</Grid>;
+  const data = useSelector((state) => state.data);
+
+  const rows = [];
+
+  if (Object.keys(data).length) {
+    for (const [key, value] of Object.entries(data.Ugovori)) {
+      rows.push({
+        id: key,
+        datum: value.datumU,
+        kvadratura: value.n[0].pov,
+        cena: value.cena,
+        cenaKvadrat: Math.ceil(value.cena / value.n[0].pov),
+      });
+    }
+  }
+
+  const columns = [
+    { field: "datum", headerName: "Datum Prodaje", width: 250 },
+    { field: "kvadratura", headerName: "Povrsina", width: 250 },
+    { field: "cena", headerName: "Cena (EUR)", width: 250 },
+    { field: "cenaKvadrat", headerName: "Cena za kvadrat", width: 250 },
+  ];
+
+  return (
+    <Grid item style={{ flexGrow: 1, padding: "1rem" }}>
+      <DataGrid rows={rows} columns={columns} autoHeight />
+    </Grid>
+  );
 };
 
 export default DataDisplay;
