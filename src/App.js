21e35f1c5ca75053111f14153d4406049ff1c282
Pedja Stojiljkovic <pedja1310@gmail.com>
2022-03-01 20:44:39 +0100

    added routing with new pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import { Provider } from "react-redux";
 
-import CssBaseline from "@mui/material/CssBaseline";
-import { Grid } from "@mui/material";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import { Grid } from "@mui/material";
 
-import Map from "./components/Map";
-import Search from "./components/Search";
-import DataDisplay from "./components/DataDisplay";
+// import Map from "./components/Map";
+// import Search from "./components/Search";
+// import DataDisplay from "./components/DataDisplay";
 
 import store from "./store";
 import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import FormPage from "./pages/FormPage";
 
 function App() {
   return (
     <>
-      <CssBaseline />
-      <Grid container direction="row">
-        <Provider store={store}>
-          <Header />
-        </Provider>
-      </Grid>
+      {/* <CssBaseline /> */}
+      <Provider store={store}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/form" element={<FormPage />} />
+        </Routes>
+      </Provider>
     </>
   );
 }
