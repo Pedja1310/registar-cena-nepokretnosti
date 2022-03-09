27628b52f7b78c612be413de2eb2f86f00f61541
Pedja Stojiljkovic <pedja1310@gmail.com>
2022-03-09 20:48:03 +0100

    improved styling for form pages to be more consistent

--- src/components/FormPage1.js
+++ src/components/FormPage1.js
@@ -13,14 +13,35 @@ const FormPage1 = ({ changeFormPage }) => {
         flexDirection: "column",
       }}
     >
-      <Typography variant="h5" sx={{ color: "white", marginBottom: "3rem" }}>
-        Izaberi lokaciju
-      </Typography>
-      <img
-        src="/images/planet-icon.png"
-        alt="Planet icon"
-        style={{ width: 100, alignSelf: "center", marginBottom: "5rem" }}
-      />
+      <Box
+        sx={{
+          marginBottom: "5rem",
+          width: "100%",
+          display: "grid",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h5" sx={{ color: "white" }}>
+          Izaberi lokaciju
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          width: "10rem",
+          height: "10rem",
+          display: "grid",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: "5rem",
+        }}
+      >
+        <img
+          src="/images/planet-icon.png"
+          alt="Planet icon"
+          style={{ width: 100, alignSelf: "center" }}
+        />
+      </Box>
       <MunicipalityPicker />
       <Button
         to="/form"
@@ -33,18 +54,26 @@ const FormPage1 = ({ changeFormPage }) => {
           marginBottom: "1rem",
           fontSize: ".7rem",
           fontWeight: 800,
+          "&:hover": {
+            backgroundColor: "#FFBB00",
+          },
         }}
         onClick={() => changeFormPage(2)}
       >
         potvrdi
       </Button>
-      <Typography
-        variant="caption"
-        sx={{ fontSize: ".6rem", color: "white", width: "85%" }}
-      >
-        U 2021. godini najveci broj prodatih stanova je bio u okviru opstine
-        Novi Beograd
-      </Typography>
+      <Box sx={{ height: "3rem", textAlign: "center", width: "85%" }}>
+        <Typography
+          variant="caption"
+          sx={{
+            fontSize: ".6rem",
+            color: "white",
+          }}
+        >
+          U 2021. godini najveci broj prodatih stanova je bio u okviru opstine
+          Novi Beograd
+        </Typography>
+      </Box>
     </Box>
   );
 };
--- src/components/FormPage2.js
+++ src/components/FormPage2.js
@@ -1,8 +1,17 @@
-import { Button, Container, Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import MunicipalityPicker from "./MunicipalityPicker";
+import PeriodPicker from "./PeriodPicker";
+import { getData } from "../store/actions/dataActions";
+import { useDispatch, useSelector } from "react-redux";
 
 const FormPage2 = ({ changeFormPage }) => {
+  const dispatch = useDispatch();
+  const { requestData } = useSelector((state) => state);
+
+  const getDataFromDB = () => {
+    dispatch(getData(requestData));
+  };
+
   return (
     <Box
       sx={{
@@ -11,34 +20,45 @@ const FormPage2 = ({ changeFormPage }) => {
         display: "flex",
         justifyContent: "center",
         flexDirection: "column",
+        flex: 1,
       }}
     >
       <Box
         sx={{
-          display: "flex",
+          display: "grid",
+          gridTemplateColumns: "1fr auto 1fr",
           alignItems: "center",
-          justifyContent: "center",
-          marginBottom: "3rem",
+          marginBottom: "5rem",
           width: "100%",
         }}
       >
-        <Button onChange={() => changeFormPage(1)}>
-          <img
-            src="/images/arrow-back-white.png"
-            alt="Back button"
-            style={{ height: "1.25rem", marginRight: "2rem" }}
-          />
+        <Button
+          sx={{ width: "1rem", padding: 0 }}
+          onClick={() => changeFormPage(1)}
+        >
+          <img src="/images/arrow-back-white.png" alt="Back button" />
         </Button>
-        <Typography variant="h6" sx={{ color: "white" }}>
+        <Typography variant="h5" sx={{ color: "white" }}>
           Izaberi Period
         </Typography>
       </Box>
-      <img
-        src="/images/date-icon.png"
-        alt="Planet icon"
-        style={{ width: 100, alignSelf: "center", marginBottom: "5rem" }}
-      />
-      <MunicipalityPicker />
+      <Box
+        sx={{
+          width: "10rem",
+          height: "10rem",
+          display: "grid",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: "5rem",
+        }}
+      >
+        <img
+          src="/images/date-icon.png"
+          alt="Planet icon"
+          style={{ width: 100, alignSelf: "center" }}
+        />
+      </Box>
+      <PeriodPicker />
       <Button
         to="/form"
         sx={{
@@ -50,18 +70,28 @@ const FormPage2 = ({ changeFormPage }) => {
           marginBottom: "1rem",
           fontSize: ".7rem",
           fontWeight: 800,
+          "&:hover": {
+            backgroundColor: "#FFBB00",
+          },
         }}
-        onClick={() => changeFormPage(3)}
+        onClick={getDataFromDB}
       >
         potvrdi
       </Button>
-      <Typography
-        variant="caption"
-        sx={{ fontSize: ".6rem", color: "white", width: "85%" }}
-      >
-        Prosecna cena kvadrata je na teritoriji Beograda porasla za 27341% u
-        odnosu na 2020. godinu
-      </Typography>
+      <Box sx={{ height: "3rem", textAlign: "center", width: "85%" }}>
+        <Typography
+          variant="caption"
+          sx={{
+            fontSize: ".6rem",
+            color: "white",
+            width: "85%",
+            textAlign: "center",
+          }}
+        >
+          Prosecna cena kvadrata je na teritoriji Beograda porasla za 27341% u
+          odnosu na 2020. godinu
+        </Typography>
+      </Box>
     </Box>
   );
 };
