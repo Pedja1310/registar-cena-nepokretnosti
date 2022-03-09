760a34ae2f57d3b7ef5320e1f623aab0f95dc754
Pedja Stojiljkovic <pedja1310@gmail.com>
2022-03-10 00:07:47 +0100

    added search result page

--- src/pages/SearchResultsPage.js
+++ src/pages/SearchResultsPage.js
@@ -1,7 +1,101 @@
-import { Button, Typography } from "@mui/material";
+import { Container, Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const SearchResultsPage = () => {
-  return <Typography>Search Results page</Typography>;
+  const { averagePrice } = useSelector((state) => state.data);
+
+  return (
+    <Container
+      maxWidth={false}
+      sx={{
+        backgroundImage: `url(${"/images/background-image.png"})`,
+        backgroundSize: "cover",
+        height: "90vh",
+        width: "100%",
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        paddingX: 0,
+        paddingTop: "4rem",
+        // alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+        // justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          width: "100%",
+          height: "50%",
+          backgroundImage: `url(${"/images/result-background.png"})`,
+          backgroundSize: "cover",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Typography
+          variant="subtitle"
+          sx={{ color: "#080F58", fontWeight: "600" }}
+        >
+          Prosecna cena kvadrata
+        </Typography>
+        <Typography
+          variant="h2"
+          sx={{
+            display: "block",
+            fontWeight: "900",
+            color: "#080F58",
+            textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+          }}
+        >
+          {averagePrice.amount}.00&euro;
+        </Typography>
+      </Box>
+      <Box sx={{ alignSelf: "center", width: "85%", marginTop: "2rem" }}>
+        <Button
+          component={Link}
+          to="/form"
+          sx={{
+            backgroundColor: "#FFBB00",
+            color: "#080F58",
+            borderRadius: 80,
+            width: "100%",
+            height: "3.5rem",
+            marginBottom: "1rem",
+
+            fontSize: ".7rem",
+            fontWeight: "bold",
+            "&:hover": {
+              backgroundColor: "#FFBB00",
+            },
+          }}
+        >
+          Detalji pretrage
+        </Button>
+        <Button
+          variant="outlined"
+          sx={{
+            borderColor: "white",
+            color: "white",
+            borderRadius: 80,
+            width: "100%",
+            height: "3.5rem",
+            fontWeight: "bold",
+            fontSize: ".7rem",
+            "&:hover": {
+              backgroundColor: "transparent",
+              borderColor: "white",
+            },
+          }}
+        >
+          Nova pretraga
+        </Button>
+      </Box>
+    </Container>
+  );
 };
 
 export default SearchResultsPage;
