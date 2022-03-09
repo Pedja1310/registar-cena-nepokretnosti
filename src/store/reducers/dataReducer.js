5bfa336fa453c2b8a113398253a4db9fe43f7797
Pedja Stojiljkovic <pedja1310@gmail.com>
2022-03-10 00:09:46 +0100

    minor fixes

--- src/store/reducers/dataReducer.js
+++ src/store/reducers/dataReducer.js
@@ -1,6 +1,10 @@
 import { GET_CONTRACTS, GET_AVERAGE_PRICE, SET_LOADING } from "../constants";
 
-const initialState = { loading: false, contracts: [], averagePrice: {} };
+const initialState = {
+  loading: false,
+  contracts: [],
+  averagePrice: {},
+};
 
 const dataReducer = (state = initialState, action) => {
   switch (action.type) {
